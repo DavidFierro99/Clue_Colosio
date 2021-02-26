3045081dd2fdbd8743eed0a184b2f31b586dea16
David Fierro <davidavid99@gmail.com>
2021-02-26 03:08:52 +0000

    First commit, program choose randomly a suspect, place and weapon

--- main.cs
+++ main.cs
@@ -1,7 +1,22 @@
 using System;
 
+
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Tijuana, ");
+
+    var rand = new Random();
+
+    Console.WriteLine ("Tijuana, 23 de marzo de 1994...");
+
+    string[] sospechosos = {"Carlos S", "Mario A", "Ernesto Z", "Comandante M", "Diego M"};
+
+    string[] lugares = {"Lomas taurinas", "Aeropuerto", "Mitin", "Sede del partido", "Coche"};
+
+    string[] armas = {"Pistola", "Cuchillo", "Playera del PRI", "Taco envenenado", "Carro de tamales"}; 
+
+    string[] asesino = {sospechosos[rand.Next(0,5)], lugares[rand.Next(0,5)], armas[rand.Next(0,5)]};
+    
+    Console.WriteLine("Colosio fue asesinado por " + asesino[0] + 
+                      " en " + asesino[1] + " con " + asesino[2]);
   }
 }
