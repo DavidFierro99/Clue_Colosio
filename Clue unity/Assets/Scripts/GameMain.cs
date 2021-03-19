1e9a50338dc5a5779af9aa0162bd8f33f36e26bb
DavidFierro99 <davidavid99@gmail.com>
2021-03-19 10:58:22 -0600

    Upload unity files

---
+++ Clue unity/Assets/Scripts/GameMain.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameMain : MonoBehaviour
+{   
+    static int PREGUNTAS_DISPONBILES = 5;
+
+    static Text n_Text;
+
+    // Categorias del juego, constante
+    static string[] DICT_ELEMENTOS = {"Sospechosos", "Lugares", "Armas"};
+    
+    // Elementos del juego, personas, lugares y armas
+    static string[,] DATOS = new string[,] {{"Carlos S", "Mario A", "Ernesto Z", "Comandante M", "Joaquin G"},
+                                            {"Lomas taurinas", "Aeropuerto", "Mitin", "Sede del partido", "Coche",},
+                                            {"Pistola", "Cuchillo", "Playera del PRI", "Taco envenenado", "Carro de tamales"}};
+    
+    public string[] ASESINO;
+
+    static string[] sospecha;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ASESINO = GameSingleton.Instance.ASESINO;
+        Debug.Log(ASESINO[0] + "/" + ASESINO[1] + "/" + ASESINO[2]);
+
+        n_Text = GameObject.Find("/Canvas/n_preguntas").GetComponent<Text>();
+        n_Text.text = PREGUNTAS_DISPONBILES.ToString();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void pregunta(string escena)
+    {
+        PREGUNTAS_DISPONBILES--;
+        SceneManager.LoadScene(escena);
+    }
+
+
+    public void volver()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    
+}
---
+++ Clue unity/Assets/Scripts/GameSingleton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSingleton : MonoBehaviour
+{
+    static string[,] DATOS = new string[,] {{"Carlos S", "Mario A", "Ernesto Z", "Comandante M", "Joaquin G"},
+                                            {"Lomas taurinas", "Aeropuerto", "Mitin", "Sede del partido", "Coche",},
+                                            {"Pistola", "Cuchillo", "Playera del PRI", "Taco envenenado", "Carro de tamales"}};
+
+    public string[] ASESINO;
+
+    private static GameSingleton _instance;
+
+    public static GameSingleton Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+            Debug.Log("Inside singleton");
+
+            ASESINO = new string[]{DATOS[0, Random.Range(0, 5)],
+                                   DATOS[1, Random.Range(0, 5)],
+                                   DATOS[2, Random.Range(0, 5)],
+                                   };
+
+            Instance = this;
+        }
+    }
+}
